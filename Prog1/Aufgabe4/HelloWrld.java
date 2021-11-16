164d5cafe62afdcbc6822b3507562d9e45581dc4
pantom2d <niklas.gerten@hs-augsburg.de>
2021-11-16 14:01:54 +0100

    Cleaned up

--- Prog1/Aufgabe4/HelloWrld.java
+++
@@ -1,8 +0,0 @@
-package Prog1.Aufgabe4;
-
-public class HelloWrld {
-    public static void main(String[] args) {
-        
-    System.out.println("Hello Wrld!");
-}
-}
--- Prog1/Aufgabe4/Test.java
+++
@@ -1,8 +0,0 @@
-package Prog1.Aufgabe4;
-
-public class Test {
-    public static void main(String[] args) {
-        System.out.println("This should show up on github!");
-    }
-    
-}
