9070181a0e6b59c840be7fd99c91b9fcf7c62cd6
pantom2d <niklas.gerten@hs-augsburg.de>
2021-11-18 21:53:33 +0100

    startet with potencies

---
+++ Prog1/Aufgabe5/Potencies.java
@@ -0,0 +1,32 @@
+package Prog1.Aufgabe5;
+
+import java.util.Scanner;
+
+public class Potencies {
+    public static void main(String[] args) {
+        int testNumber = calculateIterativ(getNumber());
+        System.out.println(testNumber);
+    }
+
+    public static int[] getNumber(){
+        int[] number = new int[2];
+        Scanner s = new Scanner(System.in);
+        System.out.println("Enter Bases");
+        number[0] = s.nextInt();
+        System.out.println("Enter Exponent");
+        number[1] = s.nextInt();
+
+        return number;
+    }
+    
+    public static int calculateIterativ(int[] input){
+        int exponent = input[1];
+        int result = input[0];
+        for(int i = 0; i < exponent-1; i++){
+            result *= input[0];
+        }
+
+        return result;
+    }
+    
+}
