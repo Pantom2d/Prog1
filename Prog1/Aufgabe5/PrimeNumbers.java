741cb37344a0e6293d14df81ecf6195aeb508eb5
pantom2d <niklas.gerten@hs-augsburg.de>
2021-11-21 14:49:55 +0100

    finished prime

--- Prog1/Aufgabe5/PrimeNumbers.java
+++ Prog1/Aufgabe5/PrimeNumbers.java
@@ -1,8 +1,25 @@
 package Prog1.Aufgabe5;
 
+import java.util.Scanner;
+
 public class PrimeNumbers {
     public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter Prime Number to check: ");
+        int numberToCheck = scan.nextInt();
+        if(isPrime(numberToCheck))
+            System.out.println(numberToCheck + " is prime!");
+        else{
+            System.out.println(numberToCheck + " is not prime!");
+        }
         
     }
-    
+
+    public static boolean isPrime(int number){
+        for (int i = 2; i < number/2; i++){
+            if (number % i == 0)
+            return false;
+        }
+        return true;
+    }
 }
